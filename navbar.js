3829177e630216f89afc4e3d8e075fb34a67b96f
Swam1234 <swamhtetpaing.2003@gmail.com>
2022-09-09 16:37:10 +0630

    destination_page-without-js-test

--- navbar.js
+++ navbar.js
@@ -1,11 +1,14 @@
 const navbarOpenBtn = document.querySelector("#navbar-open-btn");
 const navbarCloseBtn = document.querySelector("#navbar-close-btn");
 const navbar = document.querySelector("#navbar");
+const body = document.body;
 
 navbarOpenBtn.addEventListener("click" , () => {
     navbar.classList.replace('hidden' , 'flex')
+    body.classList.replace('overflow-y-auto' , 'overflow-y-hidden')
 })
 
 navbarCloseBtn.addEventListener("click" , () => {
     navbar.classList.replace('flex' , 'hidden')
+    body.classList.replace('overflow-y-hidden' , 'overflow-y-auto')
 })
