4701437330d82e5cced6424c667a131da9a54d85
Swam1234 <swamhtetpaing.2003@gmail.com>
2022-09-11 11:16:42 +0630

    destination_page-v-1-pro

--- navbar.js
+++ navbar.js
@@ -3,12 +3,13 @@ const navbarCloseBtn = document.querySelector("#navbar-close-btn");
 const navbar = document.querySelector("#navbar");
 const body = document.body;
 
-navbarOpenBtn.addEventListener("click" , () => {
-    navbar.classList.replace('hidden' , 'flex')
-    body.classList.replace('overflow-y-auto' , 'overflow-y-hidden')
+navbarOpenBtn.addEventListener("click", () => {
+    navbar.classList.replace('hidden', 'flex')
+    body.classList.replace('overflow-y-auto', 'overflow-y-hidden')
 })
 
-navbarCloseBtn.addEventListener("click" , () => {
-    navbar.classList.replace('flex' , 'hidden')
-    body.classList.replace('overflow-y-hidden' , 'overflow-y-auto')
+navbarCloseBtn.addEventListener("click", () => {
+    navbar.classList.replace('flex', 'hidden')
+    body.classList.replace('overflow-y-hidden', 'overflow-y-auto')
 })
+
